bd2bd4a26f6ea186289af4da00658705755cc9aa
josephlarrivy <joseph.larrivy@gmail.com>
2023-07-06 13:09:34 -0500

    still playing with ideas

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function isTrue(string) {
-    return string === true ? true : false
+// const useLocalStorage = () => {
+
+//   const localStoreToken = (token) => {
+//       localStorage.setItem('token', token)
+//   }
+
+//   const localRemoveToken = () => {
+//       localStorage.removeItem('token')
+//   }
+
+//   const localRetrieveToken = () => {
+//       const token = localStorage.getItem('token')
+//       return token
+//   }
+
+//   const localDecodeToken = async () => {
+//     const storedToken = localStorage.getItem('token')
+//     const decodedToken = await BackendApiRequest.decodeToken(storedToken)
+//     if (decodedToken == 'invalid') {
+//         return null;
+//     } else {
+//         return decodedToken;
+//     }
+//   }
+
+//   return [localStoreToken, localRemoveToken, localRetrieveToken, localDecodeToken];
+// };
+
+// export default useLocalStorage;
+
+
+
+const addAllNums = (numbers) => {
+  let total = 0;
+
+  for (let i=0; i<numbers.length; i++) {
+    total += numbers[i]
+  }
+  return total
 }
 
-module.exports = isTrue;
+module.exports = addAllNums;
